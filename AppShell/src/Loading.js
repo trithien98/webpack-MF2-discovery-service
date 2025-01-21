f06c8f514eb0d38c856f5a28571cc129b64300a9
Luca Mezzalira <mezzalab@gmail.com>
2025-01-21 23:00:51 +0000

    dynamic routing added refactor code to divide components from main application logic

---
+++ AppShell/src/Loading.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Loading = () => (
+  <div style={{ 
+    display: 'flex', 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    height: '50vh' 
+  }}>
+    <h2>Loading application...</h2>
+  </div>
+);
+
+export default Loading;
