15da6d3d1c78b8f3921360a58dfe48ce8e4dad0c
Luca Mezzalira <mezzalab@gmail.com>
2025-03-31 21:24:39 +0100

    Refactor event emitter implementation to use singleton pattern for improved instance management
    - Introduced EventEmitterSingleton class to ensure a single instance of the event emitter
    - Updated methods for emitting, subscribing, and unsubscribing to events
    - Enhanced code organization and encapsulation of event handling logic

--- AppShell/src/services/eventEmitter.js
+++ AppShell/src/services/eventEmitter.js
@@ -1,6 +1,29 @@
 import { EventEmitter } from 'tseep';
 
-// Create a singleton instance of the event emitter
-const emitter = new EventEmitter();
+class EventEmitterSingleton {
+    constructor() {
+        if (EventEmitterSingleton.instance) {
+            return EventEmitterSingleton.instance;
+        }
+        
+        this.emitter = new EventEmitter();
+        EventEmitterSingleton.instance = this;
+    }
+
+    emit(event, data) {
+        this.emitter.emit(event, data);
+    }
+
+    on(event, callback) {
+        this.emitter.on(event, callback);
+    }
+
+    off(event, callback) {
+        this.emitter.off(event, callback);
+    }
+}
+
+const emitter = new EventEmitterSingleton();
+Object.freeze(emitter);
 
 export default emitter; 
