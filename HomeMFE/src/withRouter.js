35a63228e42c7ee0e11cc3d32653c77a0dc54a91
Luca Mezzalira <mezzalab@gmail.com>
2025-08-14 09:05:07 +0100

    Remove legacy withRouter implementation to streamline routing in favor of React Router hooks

--- HomeMFE/src/withRouter.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-
-export function withRouter(Component) {
-  return function WithRouterComponent(props) {
-    const navigate = useNavigate();
-    return <Component {...props} navigate={navigate} />;
-  }
-} 
